47b3047303e8b2d613eaa00e71610fd1c6702ead
kubistika <kmizrachi18@gmail.com>
2019-08-02 14:28:50 +0300

    wip

--- sniffer.c
+++ sniffer.c
@@ -12,14 +12,14 @@ MODULE_LICENSE("GPL");
 /* disable page protection */
 void disable_page_protection(void)
 {
-	pr_info("DISABLE_PAGE_PROTECTION");
+	pr_info("DISABLE_PAGE_PROTECTION\n");
 	write_cr0(read_cr0() & (~0x10000));
 }
 
 /* enable page protection */
 void enable_page_protection(void)
 {
-	pr_info("ENABLE_PAGE_PROTECTION");
+	pr_info("ENABLE_PAGE_PROTECTION\n");
 	write_cr0(read_cr0() | 0x10000);
 }
 
@@ -46,20 +46,17 @@ static int (*trampoline_netif_receive_skb)(struct sk_buff *skb);
 int hook_netif_receive_skb(struct sk_buff *skb)
 {
     /* do some hook stuff */
-    pr_info("in hook!");
+    pr_info("in hook!\n");
 
-    disable_page_protection();
-    /* call patched prologe */
-    (*(void(*)())old_proluge)();
-    enable_page_protection();
-    return 0;
+    /* call original function */
+    return trampoline_netif_receive_skb(skb);
 }
 
 static int kubisti_sniffer_init(void)
 {
     disable_page_protection();
     /* fetch address of original function */
-    original_netif_receive_skb = (void*) kallsyms_lookup_name("__netif_receive_skb");
+    original_netif_receive_skb = (void*) kallsyms_lookup_name("netif_receive_skb");
 
     /* override proluge with jump instruction to our hook */
     *jump_pointer = (unsigned long) hook_netif_receive_skb;
@@ -67,14 +64,18 @@ static int kubisti_sniffer_init(void)
     /* save old proluge */
     memcpy(old_proluge, original_netif_receive_skb, ASSEMBLY_LENGTH);
 
-    /* override original function proluge with jump */
-    memcpy(original_netif_receive_skb, jump_assembly, ASSEMBLY_LENGTH);
-
     /* set trampoline pointer to original code, after the direct hook */
     *trampoline_pointer = (unsigned long) original_netif_receive_skb + ASSEMBLY_LENGTH;
 
     /* append the jump after old prologe */
     memcpy(old_proluge + ASSEMBLY_LENGTH, trampoline, ASSEMBLY_LENGTH);
+
+    /* override original function proluge with jump */
+    memcpy(original_netif_receive_skb, jump_assembly, ASSEMBLY_LENGTH);
+
+    /* set pointer to trampoline */
+    trampoline_netif_receive_skb = (void*) old_proluge;
+
     enable_page_protection();
     return 0;
 }
